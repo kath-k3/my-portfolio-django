96532c48688c09e9daea9464ba6004cac7ddbec7
kathk3 <onikka@gmail.com>
2020-06-06 10:52:59 +0200

    Removed the STATICFILES_DIRS from settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -160,9 +160,9 @@ MEDIA_URL = '/media/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 STATIC_URL = '/static/'
 
