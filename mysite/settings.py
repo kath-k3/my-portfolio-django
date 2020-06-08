343dd7b9c98217889b655a65328d0d943d388f5b
kathk3 <onikka@gmail.com>
2020-06-08 18:30:49 +0200

    Staticfiles and static dirs

--- mysite/settings.py
+++ mysite/settings.py
@@ -157,12 +157,12 @@ MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, '/static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '/static'),
+)
 
 STATIC_URL = '/static/'
 
